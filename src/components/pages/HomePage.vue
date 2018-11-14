3cfb87627da306eb9fca73949128ec13be891f60
Andy Gray <andy@whatsthescore.co.uk>
2018-11-14 15:06:21 +0000

    padding

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -7,7 +7,7 @@
         <h1 class="display-4 text-left mb-4">CryptoKaiju</h1>
         <h2>Collectible Vinyl Toys</h2>
         <p><em>Powered by the Ethereum Blockchain</em></p>
-        <p class="mt-5" v-if="totalSupply">There are currently <span class="badge badge-primary">{{ totalSupply }}</span> kaijus in existence</p>
+        <p class="mt-5" v-if="totalSupply">There are currently <span class="badge badge-primary pl-2 pr-2">{{ totalSupply }}</span> kaijus in existence</p>
       </div>
       <div class="col-sm-7 mt-5">
         <b-form @submit="searchByKId" novalidate class="">
